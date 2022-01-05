c82276c2f53177bd488280cf61c8c7a1432d6365
Rully Ihza Mahendra <rullyihza00@gmail.com>
2022-01-05 18:18:19 +0700

    Created Landing activity

---
+++ app/src/main/java/id/rllyhz/meapp/ui/landing/LandingActivity.kt
@@ -0,0 +1,12 @@
+package id.rllyhz.meapp.ui.landing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.rllyhz.meapp.R
+
+class LandingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
